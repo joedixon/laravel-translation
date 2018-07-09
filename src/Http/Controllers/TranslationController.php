6cede3104d9ef9074e2c92500c56c564a4ede32c
Joe Dixon <hello@joedixon.co.uk>
2018-07-09 22:11:16 +0100

    Initial commit of controller, and config

---
+++ src/Http/Controllers/TranslationController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace JoeDixon\Translation\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+
+class TranslationController extends Controller
+{
+}
---
+++ src/config/translation.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'route_group_config' => [
+        'middleware' => 'web',
+        'namespace' => 'JoeDixon\\Translation\\Http\\Controllers'
+    ],
+];
