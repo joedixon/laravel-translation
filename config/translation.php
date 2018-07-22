c222b7db2ab4057efec600f4f0084e6922eae1ec
Joe Dixon <hello@joedixon.co.uk>
2018-07-22 10:58:22 +0100

    Update default driver

---
+++ config/translation.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'driver' => 'file',
+
+    'route_group_config' => [
+        'middleware' => 'web',
+        'namespace' => 'JoeDixon\\Translation\\Http\\Controllers'
+    ],
+];
