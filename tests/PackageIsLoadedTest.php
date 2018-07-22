93a1577be765293b99b370b23bd3844c09987598
Joe Dixon <hello@joedixon.co.uk>
2018-07-22 10:59:23 +0100

    Add first test

---
+++ tests/PackageIsLoadedTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use Orchestra\Testbench\TestCase;
+
+class PackageIsLoadedTest extends TestCase
+{
+    protected function getPackageProviders($app)
+    {
+        return ['JoeDixon\Translation\TranslationServiceProvider'];
+    }
+
+    /** @test */
+    public function the_translation_pacakage_is_loaded()
+    {
+        $this->assertArrayHasKey('JoeDixon\Translation\TranslationServiceProvider', app()->getLoadedProviders());
+    }
+}
