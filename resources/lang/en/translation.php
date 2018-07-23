5403d3398106d9b9c834e7fded599953166591a3
Joe Dixon <hello@joedixon.co.uk>
2018-07-23 21:31:02 +0100

    Additonal language

---
+++ resources/lang/en/translation.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'languages' => 'Languages',
+    'language' => 'Language',
+    'type' => 'Type',
+    'file' => 'File',
+    'key' => 'Key',
+    'prompt_language' => 'Enter the language code you would like to add (e.g. en)',
+    'language_added' => 'New language added successfully',
+    'prompt_language_for_key' => 'Enter the language for the key (e.g. en)',
+    'prompt_type' => 'Is this a json or array key?',
+    'prompt_file' => 'Which file will this be stored in?',
+    'prompt_key' => 'What is the key for this translation?',
+    'prompt_value' => 'What is the value for this translation',
+    'type_error' => 'Translation type must be json or array',
+    'language_key_added' => 'New language key added successfully'
+];
