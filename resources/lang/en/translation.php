70155f6b0c4cc3e847bb38637bb2b2f1c2ca706e
Joe Dixon <hello@joedixon.co.uk>
2018-07-24 08:40:05 +0100

    Additonal translation keys

--- resources/lang/en/translation.php
+++ resources/lang/en/translation.php
@@ -7,12 +7,14 @@ return [
     'file' => 'File',
     'key' => 'Key',
     'prompt_language' => 'Enter the language code you would like to add (e.g. en)',
-    'language_added' => 'New language added successfully',
+    'language_added' => 'New language added successfully 🙌',
     'prompt_language_for_key' => 'Enter the language for the key (e.g. en)',
     'prompt_type' => 'Is this a json or array key?',
     'prompt_file' => 'Which file will this be stored in?',
     'prompt_key' => 'What is the key for this translation?',
     'prompt_value' => 'What is the value for this translation',
     'type_error' => 'Translation type must be json or array',
-    'language_key_added' => 'New language key added successfully'
+    'language_key_added' => 'New language key added successfully 👏',
+    'no_missing_keys' => 'There are no missing translation keys in the app 🎉',
+    'keys_synced' => 'Missing keys synchronised successfully 🎊',
 ];
