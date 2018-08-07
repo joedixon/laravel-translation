d53056b26cc4228752f18f3a3f14378a60845c42
Joe Dixon <hello@joedixon.co.uk>
2018-08-07 08:01:19 +0100

    Text input components

---
+++ resources/views/forms/text.blade.php
@@ -0,0 +1,18 @@
+<div class="input-group">
+    <label for="{{ $field }}">
+        {{ $label }}
+    </label>
+    <input 
+        class="@if($errors->has($field)) error @endif" 
+        name="{{ $field }}" 
+        id="{{ $field }}" 
+        type="text" 
+        placeholder="{{ isset($placholder) ? $placeholder : '' }}"
+        value="{{ old($field) }}"
+        {{ isset($required) ? 'required' : '' }}>
+    @if($errors->has($field))
+        @foreach($errors->get($field) as $error)
+            <p class="error-text">{!! $error !!}</p>
+        @endforeach
+    @endif
+</div>
