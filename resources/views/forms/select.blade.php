1286fd7e1366c92a88840b9c8f7926ddd6873d28
Joe Dixon <hello@joedixon.co.uk>
2018-08-09 22:39:33 +0100

    Add select blade component

---
+++ resources/views/forms/select.blade.php
@@ -0,0 +1,17 @@
+<div class="select-group">
+
+    <select name="{{ $name }}">
+        @foreach($items as $key => $value)
+            @if(is_numeric($key))
+                <option value="{{ $value }}">{{ $value }}</option>
+            @else
+                <option value="{{ $key }}">{{ $value }}</option>
+            @endif
+        @endforeach
+    </select>
+
+    <div class="caret">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+    </div>
+
+</div>
