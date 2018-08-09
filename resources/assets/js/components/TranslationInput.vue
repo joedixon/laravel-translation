b518e90270f42d0001327bc72b88e799bd4726d7
Joe Dixon <hello@joedixon.co.uk>
2018-08-09 22:38:44 +0100

    Auto focus input

--- resources/assets/js/components/TranslationInput.vue
+++ resources/assets/js/components/TranslationInput.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="flex">
-        <svg v-show="!isActive" class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
+        <svg v-show="!isActive" v-on:click="setActive" class="cursor-pointer fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
         <textarea 
             rows="1" 
             v-model="translation" 
             v-bind:class="{ active: isActive }"
             v-on:focus="setActive"
             v-on:blur="setInactive"
+            ref="trans"
         ></textarea>
     </div>
 </template>
@@ -16,13 +17,13 @@
         props: ['translation'],
         data: function() {
             return {
-                // translation: '',
                 isActive: false 
             }
         },
         methods: {
             setActive: function() {
-            this.isActive = true;
+                this.isActive = true;
+                this.$refs.trans.focus();
             },
             setInactive: function() {
                 this.isActive = false;
