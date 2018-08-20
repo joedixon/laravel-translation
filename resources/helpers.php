ae7ba011a5f78ccb9602babcacafe7a9e45886bd
Joe Dixon <hello@joedixon.co.uk>
2018-08-20 21:12:23 +0100

    String helper to match multiple haysticks

--- resources/helpers.php
+++ resources/helpers.php
@@ -6,3 +6,18 @@ if (!function_exists('set_active')) {
         return Request::is(config('translation.ui_url') . $path) ? $class : '';
     }
 }
+
+if (!function_exists('strs_contain')) {
+    function strs_contain($haystacks, $needle)
+    {
+        $haystacks = (array) $haystacks;
+
+        foreach ($haystacks as $haystack) {
+            if (str_contains(strtolower($haystack), strtolower($needle))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
