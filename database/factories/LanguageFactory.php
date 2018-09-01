4974c415dcc77126a74caa7397ee76e06f08f2be
Joe Dixon <hello@joedixon.co.uk>
2018-09-01 08:58:59 +0100

    Addd language factory

---
+++ database/factories/LanguageFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+use Faker\Generator;
+use JoeDixon\Translation\Language;
+
+$factory->define(Language::class, function (Generator $faker) {
+    return [
+        'language' => $faker->word,
+        'name' => $faker->word
+    ];
+});
