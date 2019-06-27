e4588184c30e7775939bb3f1136586aa54c5341a
Martin Kleine <martin.kleine@cunio.de>
2019-06-27 10:36:17 +0200

    Bugfix: missing fallback_locale in migration
    - added fallback_locale to the migration

--- database/migrations/2018_08_29_200844_create_languages_table.php
+++ database/migrations/2018_08_29_200844_create_languages_table.php
@@ -22,9 +22,17 @@ class CreateLanguagesTable extends Migration
                 $table->timestamps();
             });
 
-        Language::create([
-            'language' => config('app.locale'),
+        $initialLanguages = array_unique([
+            config('app.fallback_locale'),
+            config('app.locale'),
         ]);
+
+        foreach ($initialLanguages as $language) {
+            Language::create([
+                'language' => $language,
+            ]);
+        }
+
     }
 
     /**
