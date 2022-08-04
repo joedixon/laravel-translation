99f3ca7c0698178874d81420d3a4396f3ae68151
Joe Dixon <hello@joedixon.co.uk>
2022-08-04 21:58:33 +0100

    Add test stubs

---
+++ tests/NewFileDriverTest.php
@@ -0,0 +1,73 @@
+<?php
+
+test('a map of translation files can be built', function () {
+    //
+})->skip();
+
+test('a translation file can be found from the translation file map', function () {
+    //
+})->skip();
+
+test('all languages can be returned', function () {
+    //
+})->skip();
+
+test('a new language can be added', function () {
+    //
+})->skip();
+
+test('a language cannot be overwritten', function () {
+    //
+})->skip();
+
+test('all translations can be returned from a translation file', function () {
+    //
+})->skip();
+
+test('all translations can be returned from a file regardless of the depth of nesting', function () {
+    //
+})->skip();
+
+test('all translations can be returned from a file for a given language', function () {
+    //
+})->skip();
+
+test('a full list of translation keys can be found by checking all languages', function () {
+    //
+})->skip();
+
+test('an individual translation can be found for a given language', function () {
+    //
+})->skip();
+
+test('all vendor translations can be returned', function () {
+    //
+})->skip();
+
+test('both string key and short key translations can be returned', function () {
+    //
+})->skip();
+
+test('a string key translation can be saved in the correct file if it exists', function () {
+    //
+})->skip();
+
+test('a short key translation can be saved in the correct file if it exists', function () {
+    //
+})->skip();
+
+test('a string key translation can be saved in new file at the highest nesting level if a file cannot be found', function () {
+    //
+})->skip();
+
+test('a short key translation can be saved in new file at the highest nesting level if a file cannot be found', function () {
+    //
+})->skip();
+
+test('a vendor namespaced string key translation can be saved', function () {
+    //
+})->skip();
+
+test('a vendor namespaced short key translation can be saved', function () {
+    //
+})->skip();
