97ff734be38c3cc4a5f66770df0c7caa1b966036
Joe Dixon <hello@joedixon.co.uk>
2023-07-11 09:08:08 +0100

    tidy

--- src/Http/Requests/LanguageRequest.php
+++ src/Http/Requests/LanguageRequest.php
@@ -8,16 +8,6 @@ use JoeDixon\Translation\Rules\LanguageNotExists;
 class LanguageRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
